e2e77c2598987949ee239ae3f28f136e718754b5
Anvarjon <143145991+anvarjon-programmer@users.noreply.github.com>
2024-04-22 16:08:18 +0500

    I went back a lot on this project

--- src/components/tours/Main.js
+++ src/components/tours/Main.js
@@ -38,6 +38,16 @@ const Main = () => {
         </main>
       );
     }
+    if(tours.length == 0){
+        return  (
+         <main>
+            <div className='title'>
+                <h2>no tours left</h2>
+                <button className='btn' onClick={() => fetchTours()}>refresh</button>
+            </div>
+         </main>
+        )
+    }
   return (
     <main>
        <Tours tours={tours} removeTour={removeTour}/>
